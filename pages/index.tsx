95a762d186bc87b649eaaf51d2a7197ec27ae278
Arridha Amrad <arridhaamrad@gmail.com>
2022-08-31 21:42:39 +0700

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home: NextPage = () => {
   const navigate = useRouter();
+  const [name, setName] = useState("");
+  const getName = async () => {
+    const { data } = await axios.get("/api/hello");
+    setName(data.name);
+  };
+  useEffect(() => {
+    getName();
+  }, []);
   return (
     <div>
       <h1>Coba Deploy next app</h1>
+      <h2>Nama : {name}</h2>
       <button onClick={() => navigate.push("/about")}>About</button>
     </div>
   );
